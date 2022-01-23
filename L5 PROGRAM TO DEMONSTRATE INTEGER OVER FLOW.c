4af40d721ed12cfca904ee0ba640456deb279af6
Amit Kumar <94058941+w20427@users.noreply.github.com>
2022-01-23 18:49:43 +0530

    Add files via upload

---
+++ L5 PROGRAM TO DEMONSTRATE INTEGER OVER FLOW.c
@@ -0,0 +1,10 @@
+// QUESTION 05: PROGRAM TO DEMONSTRATE INTEGER OVER FLOW?
+#include <stdio.h>
+int main (){
+    int X=100;
+    int Y=50;
+    int add;
+    add=X+Y;
+    printf("%d",add);
+    return 0;
+}
